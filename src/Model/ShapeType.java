85d1cf02c23f4545931b0f8049933d031b50c149
LIA HA <129757476+yufei-ilariahuang@users.noreply.github.com>
2024-04-26 11:04:39 -0700

    Update ShapeType.java

--- src/Model/ShapeType.java
+++ src/Model/ShapeType.java
@@ -1,6 +1,8 @@
-package Model;
-
+/**
+ * Represents the types of shapes that can be drawn.
+ * This enum defines different types of shapes commonly used in graphic applications.
+ */
 public enum ShapeType {
-    LINE, CIRCLE, RECTANGLE, ELLIPSE
+  LINE, CIRCLE, RECTANGLE, ELLIPSE
 }
 
